6795ca03c577ce19f6b650311de6a959eb56e31a
Амир <50843991+Dok31@users.noreply.github.com>
2021-12-18 07:29:22 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-
+import unittest
+from main import timetable
+class Testtimetable(unittest.TestCase):
+    def test_area(self):
+        self.assertEqual(timetable('Зиганшин Амир'),'306150')
+        self.assertEqal(timetable('Гаврилов Степан Сергеевич'),'306134')
+        self.assertEqal(timetable('Вася Пупкин',''))
+    #проверка на человека
+    def test_types:
+        self.assertRaises(TypeError, timetable,'')
+        self.assertRaises(TypeError, timetable, '23+12')
+    #проверка на неправильный ввод данных
