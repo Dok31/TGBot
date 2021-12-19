0943771b1510cccea579a3484733347468d0f24e
VBUDKE <mvbudaev@miem.hse.ru>
2021-12-19 20:17:13 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -9,6 +9,7 @@ class Test(unittest.TestCase):
 
     def test_test1(self):
         self.assertEqual(test1('Зиганшин Амир'), '306150')
+        self.assertEqual(test1('Тихомиров Кирилл Сергеевич'), '')
 
 
 
